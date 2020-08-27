c235980946711a6bd6b5fb47b411d75113fd29b5
mars <mohtashim.siddiqui74@outlook.com>
2020-08-27 17:46:23 +0500

    add jwt token authentication to webapi

---
+++ webapi/webapi/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapi.Data;
+using webapi.Interfaces;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    //added by Mohtashim on 27/08/2020
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager jwtAuthenticationManager;
+
+        public AuthController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(string username, string pwd)
+        {
+            var token = jwtAuthenticationManager.Authenticate(username, pwd);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);   
+        }
+    }
+}
